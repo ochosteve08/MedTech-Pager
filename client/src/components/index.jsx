2696d1d2725f3255e61a5d9f6743ee0575d445d3
ochosteve <ochosteve08@gmail.com>
2023-07-30 18:13:24 +0100

    team-channel-list component added

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,3 +1,6 @@
 export {default as ChannelContainer} from './ChannelContainer'
 export {default as ChannelListContainer} from './ChannelListContainer'
 export {default as ChannelSearch} from './ChannelSearch'
+export {default as TeamChannelList} from './TeamChannelList'
+export {default as AddChannel} from './AddChannel'
+export {default as SearchIcon} from './SearchIcon'
