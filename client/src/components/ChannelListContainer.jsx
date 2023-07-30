e28095c941497b7a1931c09198e5c7694942db7e
ochosteve <ochosteve08@gmail.com>
2023-07-30 17:04:38 +0100

    create companyHeader function

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -19,12 +19,21 @@ const SideBar = () => (
   </div>
 );
 
+const CompanyHeader = ()=>(
+  <div className="channel-list__header">
+    <p className="channel-list__header__text">MedTech Pager</p>
+
+  </div>
+)
 
 
 const ChannelListContainer = () => {
   return (
     <>
       <SideBar />
+      <div className="channel-list__list__wrapper">
+        <CompanyHeader />
+      </div>
     </>
   );
 };
