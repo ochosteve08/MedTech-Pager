d9f043b8a176ae9f2f1fd0173e7737723c8fcabc
ochosteve <ochosteve08@gmail.com>
2023-08-01 07:34:09 +0100

    create channel inner container

--- client/src/components/ChannelInner.jsx
+++ client/src/components/ChannelInner.jsx
@@ -1,9 +1,84 @@
+import React from "react";
+import {
+  Avatar,
+  useChannelStateContext,
+  useChatContext,
+} from "stream-chat-react";
 
+import { ChannelInfo}  from "./";
 
-const ChannelInner = () => {
+export const GiphyContext = React.createContext({});
+
+// eslint-disable-next-line react/prop-types
+const ChannelInner = ({ setIsEditing }) => {
+   const { channel, watcher_count } = useChannelStateContext();
+  const { client } = useChatContext();
+
+  const MessagingHeader = () => {
+    const members =
+      channel && channel.state && channel.state.members
+        ? Object.values(channel.state.members).filter(
+            ({ user }) => user.id !== client.userID
+          )
+        : [];
+
+    const additionalMembers = members.length - 3;
+
+    if (channel?.type === "messaging") {
+      return (
+        <div className="team-channel-header__name-wrapper">
+          {members.map(({ user }, i) => (
+            <div key={i} className="team-channel-header__name-multi">
+              <Avatar
+                image={user.image}
+                name={user.fullName || user.id}
+                size={32}
+              />
+              <p className="team-channel-header__name user">
+                {user.fullName || user.id}
+              </p>
+            </div>
+          ))}
+
+          {additionalMembers > 0 && (
+            <p className="team-channel-header__name user">
+              and {additionalMembers} more
+            </p>
+          )}
+        </div>
+      );
+    }
+
+    return (
+      <div className="team-channel-header__channel-wrapper">
+        <p className="team-channel-header__name"># {channel?.data?.name}</p>
+        <span style={{ display: "flex" }} onClick={() => setIsEditing(true)}>
+          <ChannelInfo />
+        </span>
+      </div>
+    );
+  };
+
+  const getWatcherText = (watchers) => {
+    if (!watchers) return "No users online";
+    if (watchers === 1) return "1 user online";
+    return `${watchers} users online`;
+  };
+
   return (
-    <div>ChannelInner</div>
-  )
-}
+    <div className="team-channel-header__container">
+      {channel && channel.state ? (
+        <MessagingHeader channel={channel} />
+      ) : (
+        <p>Loading...</p>
+      )}
+      <div className="team-channel-header__right">
+        <p className="team-channel-header__right-text">
+          {getWatcherText(watcher_count)}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default ChannelInner
+export default ChannelInner;
