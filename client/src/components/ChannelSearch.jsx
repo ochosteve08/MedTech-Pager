7b7d22a9eededa852ceddeaec0e6802d0d274dc6
ochosteve <ochosteve08@gmail.com>
2023-08-01 08:47:59 +0100

    add AddChannel functionality to TeamChannelList component

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { useChatContext } from "stream-chat-react";
+import { useState } from "react";
+// import { useChatContext } from "stream-chat-react";
 import  SearchIcon from './SearchIcon'
 
 const ChannelSearch = () => {
